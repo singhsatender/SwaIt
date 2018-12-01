dcf5e8e93ce26bed8d7cc51fab8d76e6c4e791ef
singhsatender <32137050+singhsatender@users.noreply.github.com>
2018-12-01 13:28:08 -0500

    Initial commit

---
+++ app/src/main/java/com/ottawacoders/swapit/CreateSchedule.java
@@ -0,0 +1,13 @@
+package com.ottawacoders.swapit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateSchedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_schedule);
+    }
+}
---
+++ app/src/main/java/com/ottawacoders/swapit/WeeklySchedule.java
@@ -0,0 +1,13 @@
+package com.ottawacoders.swapit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeeklySchedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weekly_schedule);
+    }
+}
